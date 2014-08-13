4debd29fbfabe40db3d975d66955e89dab9e973c
Marc Curtis <exmonkey@gmail.com>
2014-08-13 12:11:24 +0100

    Update better_beehive_monitor_v5.ino
    New DHT22 library used (https://github.com/adafruit/DHT-sensor-library) - plus made the connection and data processing dependant on each other. Needs waaaay more work.
    The sleep function only seems to sleep for 8 mins despite setting it for 1 hour.

--- better_beehive_monitor_v5.ino
+++ better_beehive_monitor_v5.ino
@@ -1,3 +1,5 @@
+#include <DHT.h>
+
 
 //Version 4 of the beehive monitor
 
@@ -6,11 +8,12 @@
 //Uses Sleep_n0m1 Library - Noah Shibley, Michael Grant, NoMi Design Ltd. http://n0m1.com  https://github.com/n0m1/Sleep_n0m1 
 //
 
-          
+
 #include <GSM.h>                  // Importing all appropriate libraries
 #include <HttpClient.h>
 #include <Xively.h>
-#include <DHT22.h>
+//#include <DHT22.h>
+#include <DHT.h>
 #include <Sleep_n0m1.h>
 
 
@@ -20,15 +23,19 @@
 #define GPRS_LOGIN "giffgaff"            // Username (phone number)
 #define GPRS_PASSWORD "password"        // Password (phone number)
 // Setup a DHT22 nstance
-#define DHT22_PIN 4
-DHT22 myDHT22(DHT22_PIN);
-double temperature = 0; 
-double humidity = 0;
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTPIN 5
+DHT dht(DHTPIN, DHTTYPE);
+//#define DHT22_PIN 4
+//DHT22 myDHT22(DHT22_PIN);
+
+float temperature = 0; 
+float humidity = 0;
 int wait = 225;
 boolean notConnected = true;
 unsigned long time;
 int mins;
-
+boolean working;
 
 //sleep stuff
 Sleep sleep;
@@ -39,13 +46,6 @@ unsigned long sleepTime; //how long you want the arduino to sleep
 const int analogInPin = A0;    // Analog input pin that the VBAT pin is attached to
 int BatteryValue = 0;          // value read from the VBAT pin
 double outputValue = 0;        // variable for voltage calculation
-
-
-
-
-
-
-
 
 char myTempStream[] = "temperature";            // Set stream name (need to match Xively name)
 char myHumidityStream[] = "humidity";           // Set 2nd stream name
@@ -72,105 +72,71 @@ XivelyFeed feed(FEED_ID, datastreams, 3);       // Creating the feed, defining t
 XivelyClient xivelyclient(client);              // Telling Xively to use the correct client
 
 
-
 void setup(void) {                              // Connecting to network, initiating GPRS connection
   Serial.begin(9600); 
   sleepTime = 360000;//1 hour 10000;//7200000; //how long to sleep for - 2 hours
   boolean notConnected = true; //set connected value for GSM shield
+  boolean working = false;
+  dht.begin();
 
-while (notConnected) {
-
-    if(gsmAccess.begin(PINNUMBER)==GSM_READY){
-      delay(3000);
-      if(gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD)==GPRS_READY){
-        notConnected = false;
-      }
-    }
-    else{
-      delay(1000);
-    }
-  }
 }
 
 
 void loop(void) {
-  Serial.print("allow the sensor to warm up");
-  delay(5000);
 
-  Serial.print("\nVoltage: ");
-  //get data from the charger shield
-  BatteryValue = analogRead(analogInPin);            
-  // Calculate the battery voltage value
-  outputValue = (float(BatteryValue)*5)/1023*2;
-  Serial.print(outputValue);
-  Serial.print("\n---------\n");
+  if(working==false){
+    working = true;
+    Serial.print("allow the sensor to warm up");
+    delay(5000);
 
-  //connect the GSM to GPRS
-  startConnection();
+    Serial.print("\nVoltage: ");
+    //get data from the charger shield
+    BatteryValue = analogRead(analogInPin);            
+    // Calculate the battery voltage value
+    outputValue = (float(BatteryValue)*5)/1023*2;
+    Serial.print(outputValue);
+    Serial.print("\n---------\n");
 
-  //Get the sensor data
-  get_data();
+    //connect the GSM to GPRS
+    startConnection();
+  }
 
-  //add the results to the xively stream
-  datastreams[0].setFloat(temperature); 
-  datastreams[1].setFloat(humidity);
-  datastreams[2].setFloat(outputValue);
 
-  int ret = xivelyclient.put(feed, xivelyKey);  // Send to Xively
-  if(ret == 200){
-Serial.println(ret);      
-      delay(100);
-    }else{                                    
-      delay(100);
-    Serial.println(ret);
-    }
-    Serial.println("call close connecttion");  
-  closeConnection();
 }
 
 
 
 // Get the data from the DHT222 sensor
 void get_data(){
-  DHT22_ERROR_t errorCode;
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
 
-  // The sensor can only be read from every 1-2s, and requires a minimum
-  // 2s warm-up after power-on.
+  boolean gotReading = 0;
 
-  errorCode = myDHT22.readData();
-  switch(errorCode)
-  {
-  case DHT_ERROR_NONE:
-    //got some value - pass it to data function
-    processData(myDHT22.getTemperatureC(),myDHT22.getHumidity());
-
-    break;
-  case DHT_ERROR_CHECKSUM:
-    Serial.print("check sum error ");
-    Serial.print(myDHT22.getTemperatureC());
-    Serial.print("C ");
-    Serial.print(myDHT22.getHumidity());
-    Serial.println("%");
-    break;
-  case DHT_BUS_HUNG:
-    Serial.println("BUS Hung ");
-    break;
-  case DHT_ERROR_NOT_PRESENT:
-    Serial.println("Not Present ");
-    break;
-  case DHT_ERROR_ACK_TOO_LONG:
-    Serial.println("ACK time out ");
-    break;
-  case DHT_ERROR_SYNC_TIMEOUT:
-    Serial.println("Sync Timeout ");
-    break;
-  case DHT_ERROR_DATA_TIMEOUT:
-    Serial.println("Data Timeout ");
-    break;
-  case DHT_ERROR_TOOQUICK:
-    Serial.println("Polled to quick ");
-    break;
+  while (gotReading==0) {
+    humidity = dht.readHumidity();
+    temperature = dht.readTemperature();
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperature) ){
+      Serial.println("Failed to read from DHT sensor!");
+      delay(1000);
+    }
+    else{
+      gotReading = 1;
+    }
   }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  //float hi = dht.computeHeatIndex(f, h);
+
+  Serial.print("Humidity: "); 
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(temperature);
+  Serial.print(" *C ");
+  processData();
 }
 
 
@@ -184,6 +150,8 @@ void startConnection(){
       if(gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD)==GPRS_READY){
         notConnected = false;
         Serial.println("connected...");  
+        //Get the sensor data
+        get_data();
       }
     }
     else{
@@ -201,27 +169,53 @@ void closeConnection(){
       delay(1000);
       digitalWrite(3,LOW);                    // Disable the RX pin
       notConnected = true;
-      Serial.println("GSM connection closed");  
+      Serial.println("GSM connection closed");
+
+      Serial.println("Entering Sleep Mode");  
+      working = false;
+      sleep.pwrDownMode(); //set sleep mode
+      sleep.sleepDelay(sleepTime); //sleep for: sleepTime
     }
     else{
       delay(1000);
     }
   }
 
-  Serial.println("Entering Sleep Mode");  
-  sleep.pwrDownMode(); //set sleep mode
-  sleep.sleepDelay(sleepTime); //sleep for: sleepTime
+
 
 
 }
 
-void processData(double t,double h){
-  temperature = t;
-  humidity = h;
-  Serial.println(t);
-  Serial.println(h);
+void processData(){
+  Serial.println("process data"); 
+
+
+
+  //add the results to the xively stream
+  datastreams[0].setFloat(temperature); 
+  datastreams[1].setFloat(humidity);
+  datastreams[2].setFloat(outputValue);
+
+  int ret = xivelyclient.put(feed, xivelyKey);  // Send to Xively
+  Serial.println("Ret: "); 
+  Serial.println(ret); 
+
+  if(ret == 200){
+    Serial.println(ret);      
+    delay(1000);
+  }
+  else{                                    
+    delay(1000);
+    Serial.println(ret);
+  }
+
+  delay(5000);
+  Serial.println("call close connecttion");  
+
+  closeConnection();
 
 }
+
 
 
 
